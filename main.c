994a9ad05f58be1feb025aacea01d2adf33f4aa2
Makiese Morais <makiesse.dev@gmail.com>
2024-09-08 08:36:19 +0100

    update exit()

--- main.c
+++ main.c
@@ -8,18 +8,18 @@ int	main(int ac, char **av, char **env)
 	if (ac != 5)
 	{
 		ft_putstr_fd("format: ./pipex infile cmd1 cmd2 outfile\n", 2);
-		exit(-1);
+		exit(0);
 	}
 	if (pipe(pipe_fd) == -1)
 	{
 		perror("Error on init pipe!");
-		exit(-1);
+		exit(1);
 	}
 	p_id = fork();
 	if (p_id == -1)
 	{
 		perror("Error on create child!");
-		exit(-1);
+		exit(1);
 	}
 	if (p_id)
 		exec_on_parent_process(av[1], av[2], pipe_fd, env);
